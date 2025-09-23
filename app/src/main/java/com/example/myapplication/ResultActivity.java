0578200a1771a4b2a53da7a6c16d369d3785e9db
Anthony Tran <atran881@usc.edu>
2025-09-22 18:12:30 -0700

    feat: Finish end screen

--- app/src/main/java/com/example/myapplication/ResultActivity.java
+++ app/src/main/java/com/example/myapplication/ResultActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,42 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class ResultActivity extends AppCompatActivity {
 
+    private TextView resultTextView;
+    private TextView timeTextView;
+    private Button playAgainButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_result);
+
+        resultTextView = findViewById(R.id.resultTextView);
+        timeTextView = findViewById(R.id.timeTextView);
+        playAgainButton = findViewById(R.id.playAgainButton);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Intent intent = getIntent();
+        boolean gameWon = intent.getBooleanExtra("GAME_WON", false);
+        int timeUsed = intent.getIntExtra("TIME_USED", 0);
+
+        if (gameWon) {
+            resultTextView.setText(getString(R.string.game_won));
+        } else {
+            resultTextView.setText(getString(R.string.game_lost));
+        }
+
+        timeTextView.setText("Time used: " + timeUsed + " seconds");
+
+        playAgainButton.setOnClickListener(v -> {
+            Intent mainActivityIntent = new Intent(ResultActivity.this, MainActivity.class);
+            startActivity(mainActivityIntent);
+            finish();
         });
     }
 }
